d3c797fe1e827b4c944393f71a60783e51e7a9ba
Mary8992 <mc046569@gmail.com>
2022-07-06 22:35:31 +0300

    main-js modified

--- main-todo.js
+++ main-todo.js
@@ -1,11 +1,18 @@
-document.querySelector("#push").onclick = function () {
-  if (document.querySelector("#newtask input").value.length == 0) {
+let push = document.querySelector("#push");
+
+push.addEventListener("click", addItem);
+
+function addItem() {
+    let newTask = document.querySelector("#newtask input");
+    let tasks = document.querySelector("#tasks");
+
+  if (newTask.value.length == 0) {
     alert("Please Enter a Task");
   } else {
-    document.querySelector("#tasks").innerHTML += `
+    tasks.innerHTML += `
             <div class="task">
               <span id="taskname">
-                ${document.querySelector("#newtask input").value}
+                ${newTask.value}
               </span>
               <button class="delete">
                 <i class="far fa-trash-alt"></i>
@@ -13,11 +20,17 @@ document.querySelector("#push").onclick = function () {
             </div>
           `;
     //cancel button
-    var current_tasks = document.querySelectorAll(".delete");
+
+    let current_tasks = document.querySelectorAll(".delete");
+   
     for (var i = 0; i < current_tasks.length; i++) {
-      current_tasks[i].onclick = function () {
-        this.parentNode.remove();
-      };
+      current_tasks[i].addEventListener("click",removeItem);
+
+       function removeItem() {
+         if (confirm("Are you sure?")) {
+           this.parentNode.remove();
+          }
+       };
     }
   }
 };
